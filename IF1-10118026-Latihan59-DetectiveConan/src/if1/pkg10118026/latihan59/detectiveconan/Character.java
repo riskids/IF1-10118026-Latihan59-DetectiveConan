6bde3b99ba3dd60b7e3f76a7275b6621c0e901e1
Riski Dwi Sabariyanto <bintangmalamhari@gmail.com>
2019-10-27 12:25:17 +0700

    Add files via upload

---
+++ IF1-10118026-Latihan59-DetectiveConan/src/if1/pkg10118026/latihan59/detectiveconan/Character.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118026.latihan59.detectiveconan;
+
+/**
+ *
+ * @author riski
+ */
+public class Character {
+    
+    protected String name;
+    protected String from;
+    protected boolean eyeglasses;
+    
+    public Character(String name, String from, boolean eyeglasses) {
+        this.name = name;
+        this.from = from;
+        this.eyeglasses = eyeglasses;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+    
+}
---
+++ IF1-10118026-Latihan59-DetectiveConan/src/if1/pkg10118026/latihan59/detectiveconan/IF110118026Latihan59DetectiveConan.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118026.latihan59.detectiveconan;
+
+/**
+ *
+ * @author 
+ * NAMA  : Riski Dwi Sabariyanto   
+ * KELAS : IF 1
+ * NIM   : 10118026
+ * Deskripsi Program : Program ini untuk membuat objek dari anime detective 
+ *                     conan.
+ */
+public class IF110118026Latihan59DetectiveConan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        MainCharacter conan = new MainCharacter("Investigator", "Menyelesaikan Kasus","Conan Edogawa","Tokyo",true);
+        MainCharacter ran = new MainCharacter("Karate", "Sama seperti Shinichi","Ran Mouri","Osaka",false);
+        SupportCharacter eisuke = new SupportCharacter("Teman Sekelas Ran Mouri","Eisuke Hondo","Osaka",false);
+        conan.displayCharacter();
+        ran.displayCharacter();
+        eisuke.displayCharacter();
+    }
+    
+}
